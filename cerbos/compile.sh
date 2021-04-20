03fdb29557a505691df0faee9c7fdb885f040eb5
Emre <emre@qubit.com>
2021-04-20 10:12:18 +0100

    Upgrade to alpha5

--- cerbos/compile.sh
+++ cerbos/compile.sh
@@ -1,5 +1,5 @@
 docker run -i -t \
 -v $(pwd)/policies:/policies \
 -v $(pwd)/tests:/tests \
-ghcr.io/cerbos/cerbos:0.0.0-alpha4 \
+ghcr.io/cerbos/cerbos:0.0.0-alpha5 \
 compile --tests=/tests /policies
