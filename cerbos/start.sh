3d00425874b9457089f27127b43628d134295efa
Charith Ellawala <charithe@users.noreply.github.com>
2021-05-18 10:02:50 +0100

    Update to v0.0.1 (#7)

--- cerbos/start.sh
+++ cerbos/start.sh
@@ -1,5 +1,3 @@
-docker run -i -t -p 9999:9999 \
-  -v $(pwd)/config:/config \
+docker run -i -t -p 3592:3592 \
   -v $(pwd)/policies:/policies \
-  pkg.cerbos.dev/containers/cerbos:0.0.1-rc2 \
-  server --config=/config/conf.yaml
+  pkg.cerbos.dev/containers/cerbos:0.0.1
