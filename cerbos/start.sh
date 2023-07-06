7bbbed91c3b3e6331fc1132fc7157d085c326888
Charith Ellawala <charithe@users.noreply.github.com>
2023-07-06 07:48:20 +0100

    chore: Fix policy definition and start script (#36)
    - Update the derived roles policy to satisfy the new schema
    - Update the start script to make it runnable from any directory
    
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- cerbos/start.sh
+++ cerbos/start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-LATEST_VERSION=$(curl --silent "https://api.github.com/repos/cerbos/cerbos/releases/latest" | jq -r .tag_name)
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 docker run -i -t -p 3592:3592 -p 3593:3593 \
-  -v $(pwd)/policies:/policies \
-  ghcr.io/cerbos/cerbos:"${LATEST_VERSION:1}"
+  -v "${SCRIPT_DIR}/policies:/policies" \
+  ghcr.io/cerbos/cerbos:latest
